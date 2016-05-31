93b63a791ec47b2f809d16f62a000fa836339398
miawaggoner <miawaggoner@10.152.123.80>
2016-05-31 12:59:09 -0500

    Finished filters

--- src/mia/FXMLmiaController.java
+++ src/mia/FXMLmiaController.java
@@ -5,30 +5,78 @@
  */
 package mia;
 
+import com.google.gson.Gson;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import java.util.*;
-import com.google.gson.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.chart.BarChart;
+import javafx.scene.control.Slider;
 import javafx.scene.chart.XYChart;
-
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.stage.WindowEvent;
 /**
  *
  * @author csstudent
  */
 public class FXMLmiaController implements Initializable {
     
+    private Dataset data;
+    
     @FXML
     private BarChart barChart;
     
+    @FXML
+    private Slider minimum;
+    
+    @FXML
+    private Slider maximum;
+    
+    @FXML
+    private MenuBar menu;
+    
+    @FXML
+    private BorderPane bPane;
+    
+    @FXML
+    private Menu file;
+     
+    @FXML
+    private Menu help;
+    
+    @FXML
+    private MenuItem close;
+    
+    @FXML
+    private MenuItem info;
+     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         
     }
+    @FXML
+    public void close(ActionEvent event){
+        System.exit(0);
+    }
+    
+    @FXML
+    public void printAbout(ActionEvent event){
+        Alert message = new Alert(AlertType.INFORMATION);
+        message.setTitle("Information about my graph");
+        message.setHeaderText(null);
+        message.setContentText("This graph was made by Mia Swaggoner. It is about polio immunizations... ");
+        message.showAndWait();
+    }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -58,7 +106,7 @@ public class FXMLmiaController implements Initializable {
         //System.out.println(str);
         
         Gson gson = new Gson();
-        Dataset data = gson.fromJson(str, Dataset.class);
+        data = gson.fromJson(str, Dataset.class);
         
         
         Info[] infos = data.getFact();
@@ -75,9 +123,24 @@ public class FXMLmiaController implements Initializable {
         barChart.getData().add(dataSeries);
         
         
+
+    
         
-            
+        }
         
+        @FXML
+        public void handleChangeFilter(MouseEvent event){
+            barChart.getData().clear();
+            Info[] infos = data.getFact();
+            XYChart.Series<String, Number> dataSeries = new XYChart.Series();
+            for(Info info : infos){
+                if(info.getDim().getCountry() != null){
+                    if(info.getValue() <= maximum.getValue() && info.getValue() >= minimum.getValue()){
+                        dataSeries.getData().add(new XYChart.Data(info.getDim().getCountry(), info.getValue()));
+                    }
+                }
+            }
+            barChart.getData().add(dataSeries);
         
     }    
     
